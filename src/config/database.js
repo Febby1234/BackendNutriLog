b2c717ef8bba12562ff2db75fc5aff5fe759bb74
Eileen <ecynthia01@student.ciputra.ac.id>
2026-06-10 23:58:54 +0700

    initial

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  // Tambahkan baris di bawah ini untuk melewati error SSL dari Supabase
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export const query = (text, params) => pool.query(text, params);
+
+export default pool;
